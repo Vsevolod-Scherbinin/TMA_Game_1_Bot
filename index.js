d982c6a55afedb4082c090977662bf5cdafa8992
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2024-09-08 18:34:48 +0300

    TMA_Game_1_Bot - v.0.2 Mongo Connected

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const TelegramApi = require('node-telegram-bot-api');
+const mongoose = require('mongoose');
 
 const token = '6750879766:AAFr6iUUudfD_zxG6RE87VbRblR5uRrSTao';
 const ownerId = '180799659';
 
 const bot = new TelegramApi(token, {polling: true});
+
+mongoose.connect('mongodb://localhost:27017/tma_game_1');
+
+const userSchema = new mongoose.Schema({
+  userId: { type: String, unique: true },
+  score: { type: Number, default: 0 },
+});
+
+const User = mongoose.model('User', userSchema);
+
+// async function getUserCount() {
+//   try {
+//     const userCount = await User.countDocuments();
+//     console.log(`Количество пользователей: ${userCount}`)
+//     return userCount;
+//   } catch (error) {
+//     console.error('Ошибка при подсчете пользователей:', error);
+//   }
+// }
 
 const options = {
   reply_markup: JSON.stringify({
@@ -26,6 +46,12 @@ bot.on('message', async msg => {
   console.log('msg', msg);
 
   if(text.includes('start')) {
+    try {
+      // Используем метод create для создания нового пользователя
+      const newUser = await User.create({ userId });
+      // return bot.sendMessage(userId, 'Добро пожаловать! Пользователь создан.');
+    } catch {}
+
     await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/bac/a66/baca6623-5f6a-3ab2-af07-b477d91e297a/8.webp');
     return bot.sendMessage(chatId, `Добро пожаловать! Играй и заработай как можно больше очков!`, options);
   }
@@ -34,7 +60,8 @@ bot.on('message', async msg => {
   // }
 
   if(text === '!info') {
-    return bot.sendMessage(userId, `Статистика`);
+    const userCount = await User.countDocuments();
+    return bot.sendMessage(userId, `Количество пользователей: ${userCount}`);
   }
   return bot.sendMessage(chatId, `Я вас не понимаю. Попробуйте воспользоваться командами.`);
 })
