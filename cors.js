10b07cbe3c356d55cc30b777ad32505ffe1a63d4
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2024-09-12 09:32:32 +0300

    TMA_Game_1_Bot - v.0.6 Refactoring Cors

---
+++ cors.js
@@ -0,0 +1,23 @@
+const allowedCors = [
+  'http://127.0.0.1:5500',
+  'http://localhost:4000',
+  'http://localhost:3001',
+  'https://vsevolod-scherbinin.github.io',
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  res.header('Access-Control-Allow-Credentials', true);
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+};
--- index.js
+++ index.js
@@ -6,31 +6,7 @@ const app = express();
 const port = 3200;
 
 app.use(express.json());
-
-const allowedCors = [
-  'http://127.0.0.1:5500',
-  'http://localhost:4000',
-  'http://localhost:3001',
-  'https://vsevolod-scherbinin.github.io',
-];
-
-const cors = (req, res, next) => {
-  const { origin } = req.headers;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Credentials', true);
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  return next();
-};
-
+const cors = require('./cors');
 app.use(cors);
 
 app.get('/getUserData/:userId', async (req, res) => {
